dda93e419be0466cc2ee2a7860250385992b08dd
Brenon Ortega <bortega@acesso.com>
2022-02-16 23:07:37 -0300

    ADds tests for error.

---
+++ tests/Awarean.Sdk.Result.Tests/ErrorTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Awarean.Sdk.Result.Tests
+{
+    public class ErrorTests
+    {
+        [Fact]
+        public void Creating_Error_Should_Return_New_Error()
+        {
+            var error = Error.Create(code: "NEW_TEST_ERROR", reason: "Test reason for error");
+
+            error.Should().BeAssignableTo<Error>();
+        }
+
+        [Fact]
+        public void Error_Empty_Should_Have_Empty_Properties()
+        {
+            var error = Error.Empty;
+
+            error.Code.Should().Be(Error.DefaultErrorCode);
+            error.Message.Should().Be(Error.DefaultErrorMessage);
+        }
+    }
+}
