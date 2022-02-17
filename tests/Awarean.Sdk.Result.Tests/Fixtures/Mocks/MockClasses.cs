060a0de27a6a5086da6e547fffbfdfe4dd3ee47d
Brenon Ortega <bortega@acesso.com>
2022-02-16 23:07:59 -0300

    Adds tests for generic result<T>.

---
+++ tests/Awarean.Sdk.Result.Tests/Fixtures/Mocks/MockClasses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awarean.Sdk.Result.Tests.Fixtures.Mocks
+{
+    public class MockClasses
+    {
+        public class MockingClass
+        {
+            public string MockProperty1 { get; set; } = String.Empty;
+            public string MockProperty2 { get; set; } = String.Empty;
+        }
+    }
+}
---
+++ tests/Awarean.Sdk.Result.Tests/ValueResultTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using static Awarean.Sdk.Result.Tests.Fixtures.Mocks.MockClasses;
+
+namespace Awarean.Sdk.Result.Tests
+{
+    public class ValueResultTests
+    {
+        [Theory]
+        [MemberData(nameof(EmptyObjectGenerator))]
+        public void Failed_Result_Should_Have_Empty_Value_If_Available(object obj)
+        {
+            var result = Result<Error>.Fail("test_code", "test_message");
+
+            result.Value.Should().Be(Error.Empty);
+        }
+
+        [Theory]
+        [MemberData(nameof(NoEmptyObjectGenerator))]
+        public void Not_Implemented_Empty_Object_Patterns_Should_ThrowException(object obj)
+        {
+            var throwAction = new Action(() => Result<MockingClass>.Fail("test_code", "test_message"));
+
+            throwAction.Should().Throw<NotImplementedException>();
+        }
+
+        public static IEnumerable<object[]> NoEmptyObjectGenerator()
+        {
+            yield return new object[] { new MockingClass() as object };
+        }
+
+        public static IEnumerable<object[]> EmptyObjectGenerator()
+        {
+            yield return new object[] { Error.Empty as object };
+        }
+    }
+}
