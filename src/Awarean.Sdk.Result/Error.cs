b5a4dde9030f65c59e979d03dacd6fc01dc95276
Brenon Ortega <bortega@acesso.com>
2022-02-16 23:05:14 -0300

    adds consts for default code and message, fix spacing.

--- src/Awarean.Sdk.Result/Error.cs
+++ src/Awarean.Sdk.Result/Error.cs
@@ -4,15 +4,19 @@ namespace Awarean.Sdk.Result
 {
     public class Error
     {
+        public const string DefaultErrorCode = "NO_ERROR_OCURRED";
+        public const string DefaultErrorMessage = "Empty error.";
+
         private Error() { }
 
-        private Error(string code, string message) => (Code, Message) = (code, message);
+        private Error(string code, string message) => 
+            (Code, Message) = (code ?? throw new ArgumentNullException(nameof(code)), message ?? throw new ArgumentNullException(nameof(Message)));
 
         public string Code { get; private set; } = string.Empty;
         public string Message { get; private set; } = string.Empty;
 
-        public static readonly Error Empty = Create("NO_ERROR", "No error ocurred.");
-
         public static Error Create(string code, string reason) => new Error(code, reason);
+
+        public static readonly Error Empty = Create(DefaultErrorCode, DefaultErrorMessage);
     }
 }
