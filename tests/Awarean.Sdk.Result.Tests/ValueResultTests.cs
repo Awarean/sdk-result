defe5c3189c9ffa381cc91db1215947a9b8ce17e
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2022-02-26 13:06:43 -0300

    Adds test for success result and checks message.

--- tests/Awarean.Sdk.Result.Tests/ValueResultTests.cs
+++ tests/Awarean.Sdk.Result.Tests/ValueResultTests.cs
@@ -1,6 +1,5 @@
 ﻿using FluentAssertions;
 using System;
-using System.Collections.Generic;
 using Xunit;
 using static Awarean.Sdk.Result.Tests.Fixtures.Mocks.MockClasses;
 
@@ -8,32 +7,50 @@ namespace Awarean.Sdk.Result.Tests
 {
     public class ValueResultTests
     {
-        [Theory]
-        [MemberData(nameof(EmptyObjectGenerator))]
-        public void Failed_Result_Should_Have_Empty_Value_If_Available(object obj)
+        [Fact]
+        public void Failed_Result_Should_Have_Empty_Value_If_Available()
         {
             var result = Result<Error>.Fail("test_code", "test_message");
 
             result.Value.Should().Be(Error.Empty);
         }
 
-        [Theory]
-        [MemberData(nameof(NoEmptyObjectGenerator))]
-        public void Not_Implemented_Empty_Object_Patterns_Should_ThrowException(object obj)
+        [Fact]
+        public void Success_Result_Should_Correct_Value()
+        {
+            var expected = new MockingClass();
+            var result = Result<MockingClass>.Success(expected);
+
+            result.Value.Should().Be(expected);
+            result.Error.Should().Be(Error.Empty);
+        }
+
+        [Fact]
+        public void Success_Result_Should_NotBe_Empty_Object()
+        {
+            var expected = new MockingClassWithEmptyObject();
+            var result = Result<MockingClassWithEmptyObject>.Success(expected);
+
+            result.Value.Should().NotBe(MockingClassWithEmptyObject.Empty);
+        }
+
+        [Fact]
+        public void Success_Result_Should_NotBe_Null_Object()
+        {
+            var expected = new MockingClassWithNullObject();
+            var result = Result<MockingClassWithNullObject>.Success(expected);
+
+            result.Value.Should().NotBe(MockingClassWithNullObject.Null);
+        }
+
+        [Fact]
+        public void Not_Implemented_Null_Object_Patterns_Should_Require_Implementation()
         {
             var throwAction = new Action(() => Result<MockingClass>.Fail("test_code", "test_message"));
 
-            throwAction.Should().Throw<NotImplementedException>();
-        }
-
-        public static IEnumerable<object[]> NoEmptyObjectGenerator()
-        {
-            yield return new object[] { new MockingClass() as object };
-        }
-
-        public static IEnumerable<object[]> EmptyObjectGenerator()
-        {
-            yield return new object[] { Error.Empty as object };
+            throwAction.Should().Throw<NotImplementedException>()
+                .WithMessage($"No implementation of null object pattern for type {nameof(MockingClass)} found." +
+                    $"*Please implement a public static readonly field named \"Null\" or * \"Empty\" for type {nameof(MockingClass)}*");
         }
     }
 }
